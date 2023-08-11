ca8e36d6a525370bd2522984f2ade9310fa0fa42
NonelessBake <bakeferocious@gmail.com>
2023-08-11 21:26:54 +0700

    1

--- third_react/src/components/TodoTaskList.jsx
+++ third_react/src/components/TodoTaskList.jsx
@@ -2,7 +2,6 @@ import TodoTask from "./TodoTask";
 
 function TodoTaskList(newProps) {
   const { todoList } = newProps;
-  //
   return (
     <ul className="show-tasks">
       {todoList.map((task) => (
